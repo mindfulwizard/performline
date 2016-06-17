3205cfb6bf8403a5d5a5d1b0ca4a6951ff4d772f
Isaac Ratner <isaacratner@gmail.com>
2016-06-16 21:15:35 -0400

    final

--- main.js
+++ main.js
@@ -2,33 +2,30 @@ const fs = require('fs');
 const readline = require('readline');
 
 const contents = fs.readFileSync('names.txt', 'utf8');
-var names = contents.split('\n');
-var linecounter = 0;
-var output = {};
+const output = contents.split('\n').reduce((obj, elem) => {
+    obj[elem.toLowerCase()] = [];
+    return obj;
+}, {});
+
+var linecounter = 1;
 
 const rl = readline.createInterface({
     input: fs.createReadStream('list.txt', {encoding: 'utf8'})
 });
 
 rl.on('line', (line) => {
-        checker(names, line, linecounter);
+        checker(line, linecounter);
         linecounter++;
     })
     .on('close', () => {
         logger(output);
     });
 
-function checker(namesArray, phrase, lineNumber) {
-    namesArray.forEach((name) => {
-        if(name.toLowerCase() === phrase.toLowerCase()) {
-            if(output[name]) {
-                output[name].push(lineNumber)
-            } else {
-                output[name] = [];
-                output[name].push(lineNumber);
-            }
-        }
-    });
+function checker(phrase, lineNumber) {
+    var name = phrase.toLowerCase().trim();
+    if(Array.isArray(output[name])) {
+        output[name].push(lineNumber);
+    }
 }
 
 function logger(obj) {
